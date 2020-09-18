b30507d03ac80ab4dccfc21e060f7514ac26b5f3
srikanth ethiraj <srikanth.ethiraj@fireeye.com>
2020-09-18 12:30:30 -0500

    adding extra ssm parameters

--- cdk_pipeline_ecs/AppStack.py
+++ cdk_pipeline_ecs/AppStack.py
@@ -55,10 +55,10 @@ class AppStack(core.Stack):
             string_list_parameter_name="/dev/network/vpc/vpc-az"
         ).string_list_value
 
-        vpc_public_subnets = ssm.StringListParameter.from_string_list_parameter_name(
+        vpc_public_subnets = ssm.StringParameter.from_string_parameter_name(
             self, "GetVpcPublicSubnets",
-            string_list_parameter_name="/dev/network/vpc/vpc-public-subnets"
-        ).string_list_value
+            string_parameter_name="/dev/network/vpc/vpc-public-subnets"
+        ).string_value
 
         vpc_id = ssm.StringParameter.from_string_parameter_name(
             self, "GetVpcId",
@@ -69,7 +69,7 @@ class AppStack(core.Stack):
             self, "GetVpc",
             availability_zones=vpc_az,
             vpc_id=vpc_id,
-            public_subnet_ids=vpc_public_subnets
+            public_subnet_ids=[vpc_public_subnets]
         )
 
         # Get security group id from ssm parameter
