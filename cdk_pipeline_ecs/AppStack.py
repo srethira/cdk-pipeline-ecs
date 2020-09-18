5e4ac8c4b1ab18adb5d98efafb1f5ccd88d07199
srikanth ethiraj <srikanth.ethiraj@fireeye.com>
2020-09-18 09:50:36 -0500

    adding extra ssm parameters

--- cdk_pipeline_ecs/AppStack.py
+++ cdk_pipeline_ecs/AppStack.py
@@ -35,44 +35,48 @@ class AppStack(core.Stack):
 
         # These below steps allows to reuse ecs cluster which is aleady creatd by shared stack
 
-        # TODO Get cluster name from ssm parameter
+        # Get cluster name from ssm parameter
         cluster_name=ssm.StringParameter.from_string_parameter_name(
             self, "GetClusterName",
             string_parameter_name="/dev/compute/container/ecs-cluster-name"
-        )
-        # cluster_name="Prod-SharedStack-ClusterEB0386A7-iYv2Qrkn50ns"
+        ).string_value
 
-        # TODO Get vpc from ssm parameter
+        # # TODO Get vpc from ssm parameter
         # vpc_name="shared-pipeline/Prod/SharedStack/Vpc"
 
-        # Get vpc from lookup attributes
+        # # Get vpc from lookup attributes
         # ec2_vpc = ec2.Vpc.from_lookup(
         #     self, "GetVpc", 
         #     vpc_name=vpc_name
         # )
 
-        vpc_az = ssm.StringParameter.from_string_parameter_name(
+        vpc_az = ssm.StringListParameter.from_string_list_parameter_name(
             self, "GetVpcAz",
-            string_parameter_name="/dev/network/vpc/vpc-az"
-        )
+            string_list_parameter_name="/dev/network/vpc/vpc-az"
+        ).string_list_value
+
+        vpc_public_subnets = ssm.StringListParameter.from_string_list_parameter_name(
+            self, "GetVpcPublicSubnets",
+            string_list_parameter_name="/dev/network/vpc/vpc-public-subnets"
+        ).string_list_value
 
         vpc_id = ssm.StringParameter.from_string_parameter_name(
             self, "GetVpcId",
             string_parameter_name="/dev/network/vpc/vpc-id"
-        )
+        ).string_value
 
         ec2_vpc = ec2.Vpc.from_vpc_attributes(
             self, "GetVpc",
             availability_zones=vpc_az,
-            vpc_id=vpc_id
+            vpc_id=vpc_id,
+            public_subnet_ids=vpc_public_subnets
         )
 
-        # TODO Get security group id from ssm parameter
+        # Get security group id from ssm parameter
         security_group_id=ssm.StringParameter.from_string_parameter_name(
             self, "GetSgId",
             string_parameter_name="/dev/network/vpc/security-group-id"
-        )
-        # security_group_id="sg-056caf7401bfe8493"
+        ).string_value
 
         # Get security group from lookup
         ec2_sgp = ec2.SecurityGroup.from_security_group_id(
