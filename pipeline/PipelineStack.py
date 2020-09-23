dacd8b4159d00ac4bcd540a6c481defaa87c10c4
srikanth ethiraj <srikanth.ethiraj@fireeye.com>
2020-09-23 16:20:25 -0500

    updating cdk.context.json

--- pipeline/PipelineStack.py
+++ pipeline/PipelineStack.py
@@ -58,6 +58,12 @@ class PipelineStack(Stack):
             )
         )
 
+        testing_stage.add_actions(ShellScriptAction(action_name='integration', commands=[
+                'python -m test/test_handler.py',
+            ], 
+            additional_artifacts=[source_artifact]
+        )
+
         # Do this as many times as necessary with any account and region
         # Account and region may be different from the pipeline's.
         pipeline.add_application_stage(ApplicationStage(self, 'Prod',
